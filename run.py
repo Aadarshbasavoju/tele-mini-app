f55c98db3e6f968edba30d306d00f53fc7699564
0xAadarsh <74522137+Aadarshbasavoju@users.noreply.github.com>
2024-07-14 15:50:22 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import app
 from app.bot import run_bot
+from threading import Thread
 
 if __name__ == "__main__":
-    run_bot()
-    app.run(debug=True)
+    # Start the Telegram bot in a separate thread
+    bot_thread = Thread(target=run_bot)
+    bot_thread.start()
+    
+    # Start the Flask application
+    app.run(host='0.0.0.0', port=5000, debug=True)
