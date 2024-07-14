8b93499ba7f064dfc5219835bc1aee152e92de39
0xAadarsh <74522137+Aadarshbasavoju@users.noreply.github.com>
2024-07-14 16:20:57 +0530

    Update init.py

--- app/init.py
+++ app/init.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
-from app import models, bot, handlers
+from app import models, bot, handlers, routes
